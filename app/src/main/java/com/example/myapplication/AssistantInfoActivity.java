ad15c32e5d287392d1c857e65642289ee91edd3f
wahaj-yousef <77795477+wahaj-yousef@users.noreply.github.com>
2021-03-27 13:24:27 +0300

    main activty has been edited

--- app/src/main/java/com/example/myapplication/AssistantInfoActivity.java
+++ app/src/main/java/com/example/myapplication/AssistantInfoActivity.java
@@ -1,14 +1,100 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class AssistantInfoActivity extends AppCompatActivity {
+
+    Button aSave;
+    EditText message1,message2,message3, PhoneNoA;
+    HashMap<String,String> userMap = new HashMap<>();
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assistant_info);
+
+        message1=findViewById(R.id.M1New);
+        message2=findViewById(R.id.M2New);
+        message3=findViewById(R.id.M3New);
+        PhoneNoA=findViewById(R.id.phoneNooNew);
+        aSave=findViewById(R.id.SaveAssistantInfoNew);
+
+
+        //database:
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference  = firebaseDatabase.getReference("userData");
+
+
+        aSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(AssistantInfoActivity.this,MainActivity12.class);
+
+                String pha = PhoneNoA.getText().toString();
+                intent.putExtra("aPhonenum",pha);
+
+                if (pha.length() != 10){
+                    Toast.makeText(getApplicationContext(), " Write a valid number please" , Toast.LENGTH_LONG).show();
+                }else if(pha.charAt(0) == '0'){
+                    pha  = "+966" +  pha.substring(1);
+                }
+
+                String msg1 = message1.getText().toString();
+
+                String msg2 = message2.getText().toString();
+
+                String msg3 = message3.getText().toString();
+
+                if (msg1.isEmpty()){
+                    msg1 = "I'm not in in hurry but can you please if you are free come to help me with something ";
+                }else if (msg2.isEmpty()){
+                    msg2 = " I'm in wired situation Im not sure but if you are free come to me please  ";
+                }else if (msg3.isEmpty()){
+                    msg3 = "!!!  Im in very Dangers situation come and help me please ";
+                }
+
+                userMap.put("aPhonenum" , pha);
+
+                userMap.put("aMsg1" ,msg1);
+                userMap.put("aMsg2" , msg2);
+                userMap.put("aMsg3" , msg3);
+
+                firebaseDatabase.getReference("userData/anas").setValue(userMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("dataBaseError" , e.toString());
+                    }
+                });
+
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/PersonalInfoActivity.java
+++ app/src/main/java/com/example/myapplication/PersonalInfoActivity.java
@@ -1,20 +1,101 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class PersonalInfoActivity extends AppCompatActivity {
 
-    Button psave;
-    EditText Fname,Lname,phonenum;
+    Button pSave;
+    String FName,LName,phonenum;
+    EditText FirstName,LastName,PhoneNo;
+    ImageView profileimg;
+    Bitmap b;
+    HashMap<String,String> userMap = new HashMap<>();
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+
+        pSave=findViewById(R.id.SavePersonalInfoNew);
+        LastName=findViewById(R.id.LastNameNew);
+        FirstName=findViewById(R.id.FirstNameNew);
+        PhoneNo=findViewById(R.id.PhoneNoNew);
+        profileimg=findViewById(R.id.profile_imageNew);
+
+
+        //database:
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference  = firebaseDatabase.getReference("userData");
+
+
+
+
+        pSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PersonalInfoActivity.this , MainActivity12.class);
+                FName= FirstName.getText().toString();
+                LName =LastName.getText().toString();
+                phonenum= PhoneNo.getText().toString();
+                profileimg.setDrawingCacheEnabled(true);
+                b=profileimg.getDrawingCache();
+
+                if (phonenum.length() != 10){
+                    Toast.makeText(getApplicationContext(), " Write a valid number please" , Toast.LENGTH_LONG).show();
+
+                }else if(phonenum.charAt(0) == '0'){
+                    phonenum  = "+966" +  phonenum.substring(1);
+
+
+
+                    userMap.put("plastname" , LName);
+                    userMap.put("pphonenum" , phonenum);
+                    userMap.put("pfirstname" , FName);
+
+                    firebaseDatabase.getReference("userData/anas").setValue(userMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+
+
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("dataBaseError" , e.toString());
+                        }
+                    });
+
+
+
+
+                    startActivity(intent);
+                }
+
+
+            }
+        });
 
 
 
